67759a0d6e25864ab842faa0b5a5f713e445340f
Yurii Babiuk <90417593+MrYuriy@users.noreply.github.com>
2026-06-12 00:15:11 +0200

    Try to deploy

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,4 +10,4 @@ echo "Seeding roles and admin user..."
 python -m scripts.seed_data
 
 echo "Starting FastAPI application..."
-exec uvicorn app.main:app --host 0.0.0.0 --port 8000 --reload
+exec uvicorn app.main:app --host 0.0.0.0 --port "${PORT:-8000}"
