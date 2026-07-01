2b8e928d706e6b22095bd43d51d82f8ec07bc324
Yurii Babiuk <90417593+MrYuriy@users.noreply.github.com>
2026-07-02 00:21:11 +0200

    Try to fix timezone

--- app/models/base.py
+++ app/models/base.py
@@ -9,7 +9,7 @@ Base = declarative_base()
 
 class CreatedAtMixin:
     created_at = Column(
-        DateTime,
+        DateTime(timezone=True),
         default=func.now(),
         server_default=func.now(),
         index=True,
@@ -18,7 +18,7 @@ class CreatedAtMixin:
 
 class UpdatedAtMixin:
     updated_at = Column(
-        DateTime,
+        DateTime(timezone=True),
         default=func.now(),
         server_default=func.now(),
         onupdate=func.now(),
